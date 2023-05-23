2de301d07e1fd2c4719046324a359b663771e2ba
SeungYn <kwls0407@gmail.com>
2023-05-24 00:34:23 +0900

    feat: add class validation to the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from './common/util/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const PORT = config.host.port;
   await app.listen(PORT);
 }
