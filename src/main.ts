2dc66f93a0b342636680befe2370533fd566ccb9
SeungYn <kwls0407@gmail.com>
2023-05-24 03:02:13 +0900

    feat: register SuccessInterceptor in the app

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { config } from './common/util/config';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from './common/intercaptors/success.intercaptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // class validation 등록
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new SuccessInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   const PORT = config().host.port;
   await app.listen(PORT);
