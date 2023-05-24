b32110c8693b0cd534d1cf7874ca48985178833a
SeungYn <kwls0407@gmail.com>
2023-05-25 02:34:16 +0900

    feat: write cats service code
    - 캣 서비스 코드 작성
    - 몽구스의 virtual 필드를 이용해서 필터 적용

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CatRequestDto } from 'src/dto/cats.request.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
   @Get()
   getCurrentCat() {
     return 'current cat';
@@ -11,7 +14,7 @@ export class CatsController {
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     console.log(body);
-    return 'signup';
+    return await this.catsService.signUp(body);
   }
 
   @Post('login')
--- src/cats/cats.schema.ts
+++ src/cats/cats.schema.ts
@@ -30,6 +30,17 @@ export class Cat extends Document {
 
   @Prop()
   imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+// virtual 필드 실제로 저장되는 필드는 아니지만 비즈니스 로직에서 사용할 수 있도록 제공
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
