bf41ef0352242b56b9b4f62b979d59713a0abbe3
SeungYn <kwls0407@gmail.com>
2023-05-25 03:06:21 +0900

    feat: modify cat dto

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,32 +1,48 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CatRequestDto } from 'src/dto/cats.request.dto';
 import { CatsService } from './cats.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ReadOnlyCatDto } from 'src/dto/cat.dto';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+  @ApiOperation({ summary: '회원정보' })
   @Get()
   getCurrentCat() {
     return 'current cat';
   }
 
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: ReadOnlyCatDto,
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     console.log(body);
     return await this.catsService.signUp(body);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut() {
     return 'logout';
   }
 
+  @ApiOperation({ summary: '이미지 업로드' })
   @Post('upload/cats')
   uploadCatImg() {
     return 'uploadImg';
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { config } from './common/util/config';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from './common/intercaptors/success.intercaptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new SuccessInterceptor());
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = config().host.port;
   await app.listen(PORT);
 }
