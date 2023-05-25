2c42dfd9f3fd6115dcb0f3cfd1d6dd80d519b13f
SeungYn <kwls0407@gmail.com>
2023-05-25 14:08:10 +0900

    feat: write authService
    - jwt를 생성하여 반환하도록 auth service 작성
    - nestjs/jwt 안에 있는 jwtService를 사용하여 만듦

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CatsRepository } from 'src/cats/cats.repository';
 import { LoginRequestDto } from './dto/login.request.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly catRepository: CatsRepository) {}
+  constructor(
+    private readonly catsRepository: CatsRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async jwtLogin(data: LoginRequestDto) {
     const { email, password } = data;
+
+    const cat = await this.catsRepository.findCatByEmail(email);
+
+    if (!cat) {
+      throw new UnauthorizedException('이메일과 비밀번호를 확인해 주세요');
+    }
+
+    const isPasswordValidated: boolean = await bcrypt.compare(
+      password,
+      cat.password,
+    );
+
+    if (!isPasswordValidated) {
+      throw new UnauthorizedException('이메일과 비밀번호를 확인해 주세요');
+    }
+
+    // nest/jwt에 있는 api를 사용해서 jwt를 만들어야함
+
+    const payload = { email: email, sub: cat.id };
+
+    return { token: this.jwtService.sign(payload) };
   }
 }
